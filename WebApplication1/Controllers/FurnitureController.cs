f9e09b34602f982175214042580a8695c43b4704
Zubeydali <zubeydaalizada@gmail.com>
2024-11-25 19:57:45 +0400

    commit

---
+++ WebApplication1/Controllers/FurnitureController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class FurnitureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
